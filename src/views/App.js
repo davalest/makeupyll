e89a3f2831eb6ad87a483ec7d406b54ee92e7e67
David <david.valenciano@securitas.es>
2022-09-12 18:31:16 +0200

    first changes

---
+++ src/views/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+        <p>Esta es mi pagina web</p>
+    </div>
+  );
+}
+
+export default App;
