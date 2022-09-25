1b677cd619e5573f2de7803281b52e5173f4f975
Hector <donsalmeron@gmail.com>
2022-09-25 19:12:13 +0200

    add material:

--- src/views/App.js
+++ src/views/App.js
@@ -1,10 +1,17 @@
+import Button from '@mui/material/Button';
+import DrawerAppBar from '../components/Appbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
-        <p>Esta es mi pagina web</p>
+      <h1>Makeup YLL</h1>
+        <p>Maquillaje nupcial</p>
+        <Button variant="Portfolio">Portfolio</Button> 
+        <Button variant="Sobre mi">Sobre mi</Button> 
+        <Button variant="Contacto">contacto</Button> 
+        <DrawerAppBar></DrawerAppBar>
+        <h1>Yola Gayola</h1>
     </div>
   );
 }
